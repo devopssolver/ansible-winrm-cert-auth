313541220f06cf0ca10ddcb318672686c6ca0ff4
SKY <saqibyawar@gmail.com>
2020-05-25 22:51:51 -0400

    feat: map the client cert to user

--- windows_host/create_ansible_user.ps1
+++ windows_host/create_ansible_user.ps1
@@ -33,3 +33,19 @@ $newItemParams = @{
     Force        = $true
 }
 $null = New-ItemProperty @newItemParams
+
+# Map generated certificates to the ansible runner
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ansibleRunnerUsername, $ansibleRunnerPassword
+
+# Find the cert thumbprint for the client certificate created on the Ansible host
+$ansibleCert = Get-ChildItem -Path 'Cert:\LocalMachine\Root' | Where-Object {$_.Subject -eq 'CN=ansiblerunner'}
+
+$params = @{
+	Path = 'WSMan:\localhost\ClientCertificate'
+	Subject = "$ansibleRunnerUsername@localhost"
+	URI = '*'
+	Issuer = $ansibleCert.Thumbprint
+  Credential = $credential
+	Force = $true
+}
+New-Item @params
