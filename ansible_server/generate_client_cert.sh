d0a603be2f5b6b0397562f14e17b8c269ef8f47f
SKY <saqibyawar@gmail.com>
2020-05-25 22:05:17 -0400

    fix: added proper cert ignore

--- ansible_server/generate_client_cert.sh
+++ ansible_server/generate_client_cert.sh
@@ -1,2 +1,4 @@
 #/bin/bash
+
+# generate the certs
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -out client_cert.pem -outform PEM -keyout client_key.pem -subj "/CN=ansiblerunner" -extensions v3_req_client
